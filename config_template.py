0a6b7472f3a196842b985f5b86497917d63bba5c
rbharat17 <rallabandibharatchandra@gmail.com>
2025-08-11 18:41:15 +0530

    Add config template for API key

---
+++ config_template.py
@@ -0,0 +1,5 @@
+# config_template.py
+# Copy this file to config.py and replace with your real API key
+# Never commit config.py to GitHub (it’s ignored via .gitignore)
+
+VULNERS_API_KEY = "V3M7JURYREPAD5IUP4ZZB2B7ZL1747FJVXSIA81ZGQXSIH86JD23X0OJ54ZMJWDA"
